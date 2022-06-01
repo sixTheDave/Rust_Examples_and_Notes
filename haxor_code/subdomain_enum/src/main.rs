0a150a27b7ea2e239d6f17499c1f2ca53aab192a
six <51x@keemail.me>
2022-06-01 23:50:37 +0200

    haxor rust

---
+++ haxor_code/subdomain_enum/src/main.rs
@@ -0,0 +1,81 @@
+
+// Error handling
+use crate::{
+    model::{CrtShEntry, Subdomain},
+    Error,
+};
+
+// reqwest
+use reqwest::blocking::Client;
+use std::{collections::HashSet, time::Duration};
+use trust_dns_resolver::{
+    config::{ResolverConfig, ResolverOpts},
+    Resolver,
+};
+
+// Enumerates through API
+pub fn enumerate(http_client: &Client, target: &str) -> Result<Vec<Subdomain>, Error> {
+    let entries: Vec<CrtShEntry> = http_client
+        .get(&format!("https://crt.sh/?q=%25.{}&output=json", target))
+        .send()?
+        .json()?;
+
+    // clean and dedup results
+    let mut subdomains: HashSet<String> = entries
+        .into_iter()
+        .flat_map(|entry| {
+            entry
+                .name_value
+                .split('\n')
+                .map(|subdomain| subdomain.trim().to_string())
+                .collect::<Vec<String>>()
+        })
+        .filter(|subdomain: &String| subdomain != target)
+        .filter(|subdomain: &String| !subdomain.contains('*'))
+        .collect();
+    subdomains.insert(target.to_string());
+
+    let subdomains: Vec<Subdomain> = subdomains
+        .into_iter()
+        .map(|domain| Subdomain {
+            domain,
+            open_ports: Vec::new(),
+        })
+        .filter(resolves)
+        .collect();
+
+    Ok(subdomains)
+}
+
+// Resolve to ip
+pub fn resolves(domain: &Subdomain) -> bool {
+    let mut opts = ResolverOpts::default();
+    opts.timeout = Duration::from_secs(4);
+
+    let dns_resolver = Resolver::new(
+        ResolverConfig::default(),
+        opts,
+    )
+    .expect("subdomain resolver: building DNS client");
+    dns_resolver.lookup_ip(domain.domain.as_str()).is_ok()
+}
+
+
+fn main() -> Result<(), anyhow::Error> {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        return Err(Error::CliUsage.into());
+    }
+
+    let target = args[1].as_str();
+
+    let http_timeout = Duration::from_secs(5);
+    let http_client = Client::builder()
+        .redirect(redirect::Policy::limited(4))
+        .timeout(http_timeout)
+        .build()?;
+
+    //enumerate(&http_client, target)
+    // TBF
+}
